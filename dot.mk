4758d3cfe787ebcdfb688ab0c7ecda3d285ce7dd
Ujjawal Agrawal <uvagrawal123@gmail.com>
2017-10-01 18:15:01 +0530

    Dot OS bring up

--- dot.mk
+++ dot.mk
@@ -21,9 +21,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/zuk/z2_plus/device.mk)
 
 # Inherit some common AOSP-OMS stuff.
-$(call inherit-product, vendor/ownrom/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common_full_phone.mk)
 
-PRODUCT_NAME := ownrom_z2_plus
+PRODUCT_NAME := dot_z2_plus
 PRODUCT_DEVICE := z2_plus
 PRODUCT_MANUFACTURER := ZUK
 PRODUCT_BRAND := ZUK
